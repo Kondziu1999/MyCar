6ca189fc8a3f2f4188c0a28a90cb2bd85372ac12
Your Name <you@example.com>
2020-02-15 15:47:50 +0100

    add user service car, service which manage users data

---
+++ user-car-service/src/main/java/com/KOndziu/usercarservice/UserCarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.KOndziu.usercarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCarServiceApplication.class, args);
+	}
+
+}
