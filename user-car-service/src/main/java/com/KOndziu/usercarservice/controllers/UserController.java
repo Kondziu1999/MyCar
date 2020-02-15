c8be6fbe48e304a13f5c1875cf58add842476abf
Your Name <you@example.com>
2020-02-15 22:53:24 +0100

    added config server with remote git repo

--- user-car-service/src/main/java/com/KOndziu/usercarservice/controllers/UserController.java
+++ user-car-service/src/main/java/com/KOndziu/usercarservice/controllers/UserController.java
@@ -3,12 +3,14 @@ package com.KOndziu.usercarservice.controllers;
 
 import com.KOndziu.usercarservice.modules.User;
 import com.KOndziu.usercarservice.repos.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.function.Supplier;
 
+@RefreshScope
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -16,11 +18,17 @@ public class UserController {
     private final UserRepository userRepository;
     private Supplier<User> emptyUserSupp=()-> new User("not found","not found");
 
-    @Autowired
+    @Value("${my.app}")
+    private String app;
+
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    @GetMapping("/test")
+    public String getTest(){
+        return app;
+    }
     @GetMapping("/{userId}")
     public User getUserById(@PathVariable Integer userId){
         Optional<User> userOptional=userRepository.findById(userId);
